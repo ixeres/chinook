ea7afe3bedb910258ae985c717a0b7af54aad11c
Devin Leduc <ixeres@gmail.com>
2017-04-13 10:40:53 -0400

    Make the answers. Might have... Neglected to commit before now. Am sorry.

--- activerecord_assignment.rb
+++ activerecord_assignment.rb
@@ -7,25 +7,30 @@
 # 1a) Find the genre with the name "Hip Hop/Rap".
 
 
+Genre.find_by(name: 'Hip Hop/Rap')
+  Genre Load (1.2ms)
 
 # 1b) Count how many tracks belong to the "Hip Hop/Rap" genre
 
-
+Track.where(genre_id: 17).count
 
 # 2) Find the total amount of time required to listen to all the tracks in the database.
 
-
+Track.sum("milliseconds")
+   (6.9ms)  SELECT SUM("tracks"."milliseconds") FROM "tracks"
+=> 1378575827
 
 # 3a) Find the highest price of any track that has the media type "MPEG audio file".
 
-
+Track.where(media_type_id: MediaType.find_by(name: 'MPEG audio file')).order(unit_price: :desc).limit(1).first #would you have to sanitize this input?
 
 # 3b) Find the name of the most expensive track that has the media type "MPEG audio file".
 
-
+=> #<Track id: 6, album_id: 1, genre_id: 1, media_type_id: 1, name: "Put The Finger On You", composer: "Angus Young, Malcolm Young, Brian Johnson", milliseconds: 205662, bytes: 6713451, unit_price: #<BigDecimal:7fe4e58e3128,'0.99E0',9(18)>, created_at: "2005-01-03 05:49:26", updated_at: "2014-01-29 22:14:56">
 
 # 4) Find the 2 oldest artists.
 
+Artist.order(created_at: :asc).limit(2)
 
 
 ### Stretch Exercise (Active Record Query Interface)
